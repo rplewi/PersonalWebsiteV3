df4a07509e692e74cbdb6e9e5954b625c71e6ad7
rplewi <rlewi0506@gmail.com>
2025-03-04 15:17:13 -0600

    updated Projects section

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -16,15 +16,15 @@ export default function Project({name, desc, delay, height, width, githubLink, l
 
     return (
         <div ref = {ref}>
-        <div style = {{height:height, width: width}} className = "bg-color4 p-6 rounded-2xl text-color2 hover:-translate-y-2 flex flex-col">
-            <h className = "text-2xl border">{name}</h>
+        <div style = {{height:height, width: width}} className = "bg-color4 p-6 rounded-2xl text-color2 hover:-translate-y-2 flex flex-col font-rubik">
+            <h className = "text-2xl font-bold italic">{name}</h>
             <p> {desc} </p>
-            <a className = "border h-14 w-14" href="githubLink">
-                <button ><FaGithub className = "hover:text-white "size = "50"/></button>
+            <a className = "h-14 w-14" href={githubLink}>
+                <button ><FaGithub className = "hover:text-white "size = "50" /></button>
             </a>
             
         </div>   
-        <h className = "text-3xl text-white ">Languages: {languages}</h>
+            <h className = "text-2xl text-white ">Technologies: {languages}</h>
         </div>
     )
 }
